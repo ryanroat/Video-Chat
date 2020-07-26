048fecdff5a88f11726f44b5079463e55d7557a1
Ryan Roat <ryanroat@gmail.com>
2020-07-26 18:57:08 -0400

    capture/display client AV stream

--- public/script.js
+++ public/script.js
@@ -1,8 +1,28 @@
 const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 });
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
+
+navigator.mediaDevices
+    .getUserMedia({
+        video: true,
+        audio: true
+    })
+    .then(stream => {
+        addVideoStream(myVideo, stream);
+    });
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
