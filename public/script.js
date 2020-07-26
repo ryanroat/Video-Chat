c3d94a31c47634b1e8f8476cb03d98b76f1def8b
Ryan Roat <ryanroat@gmail.com>
2020-07-26 19:58:48 -0400

    return AV stream when user connects

--- public/script.js
+++ public/script.js
@@ -15,6 +15,19 @@ function addVideoStream(video, stream) {
     videoGrid.append(video);
 }
 
+function connectToNewUser(userId, stream) {
+    console.log(`connecting ${userId}`);
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', newUserStream => {
+        console.log(`calling ${userId}`);
+        addVideoStream(video, newUserStream);
+    });
+    call.on('close', () => {
+        video.remove();
+    });
+}
+
 navigator.mediaDevices
     .getUserMedia({
         video: true,
@@ -22,12 +35,20 @@ navigator.mediaDevices
     })
     .then(stream => {
         addVideoStream(myVideo, stream);
+
+        myPeer.on('call', call => {
+            console.log(`answering ${call}`);
+            // console.log('answering call');
+            call.answer(stream);
+        });
+
+        socket.on('user-connected', userId => {
+            console.log(`user connected`);
+            connectToNewUser(userId, stream);
+        });
     });
 
 myPeer.on('open', id => {
+    // console.log('join room');
     socket.emit('join-room', ROOM_ID, id);
-});
-
-socket.on('user-connected', userId => {
-    console.log(`User connected: ${userId}`);
 });
